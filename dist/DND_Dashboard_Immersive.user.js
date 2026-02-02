6f3ca7105c10c9b6a7336bccef3f0e87e5ba61c0
niccolecantdoit-rgb <niccolecantdoit@gmail.com>
2026-02-02 09:14:29 +0800

    build: bump version to 1.5.1 and rebuild artifacts

--- dist/DND_Dashboard_Immersive.user.js
+++ dist/DND_Dashboard_Immersive.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DND 沉浸式仪表盘
 // @namespace    http://tampermonkey.net/
-// @version      1.4.0
+// @version      1.5.1
 // @description  专为 DND 模板设计的游戏风格仪表盘
 // @author       Niccole
 // @match        */*
