8f15a770f13b6b04d3279eb95e1a5cf68e3acffe
niccolecantdoit-rgb <niccolecantdoit@gmail.com>
2026-02-06 15:55:48 +0800

    chore: release version 1.8.0

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DND 沉浸式仪表盘
 // @namespace    http://tampermonkey.net/
-// @version      1.7.0
+// @version      1.8.0
 // @description  专为 DND 模板设计的游戏风格仪表盘
 // @author       Niccole
 // @match        */*
